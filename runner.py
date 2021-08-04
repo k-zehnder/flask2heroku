19d2f4d3c11826895da9ee664943fc3f637b69f2
kevin <righttrianglesrkewl3@gmail.com>
2021-08-04 15:02:57 -0700

    trying4

--- runner.py
+++ runner.py
@@ -6,7 +6,7 @@ app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Hero': Patient}
+    return {'db': db, 'Patient': Patient}
 
 if __name__ == '__main__':
     app.run(debug=True)
