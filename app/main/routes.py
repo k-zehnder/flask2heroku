27e6ad729d117412da2bea95249b85b49c863711
kevin <righttrianglesrkewl3@gmail.com>
2021-08-06 12:30:25 -0700

    creds

--- app/main/routes.py
+++ app/main/routes.py
@@ -28,7 +28,9 @@ db.connect()
 db.drop_tables([Patient])
 db.create_tables([Patient])
 
-cred_json = "/home/batman/Desktop/flask2heroku/data/key.json"
+# cred_json = "/home/batman/Desktop/flask2heroku/data/key.json"
+cred_json = "data/key.json"
+
 gsh = GoogleSheetHelper(cred_json, "users_test")
 
 df = gsh.getDataframe("users_test")
